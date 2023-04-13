b2afff80d0c579f043b713d7968269e339253ae7
Ramprasadya <yadavramprasad563@gmail.com>
2023-04-13 14:37:58 +0530

    seted route and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import {  Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Login from "./screens/Login";
+
 
 function App() {
   return (
-    <div>Hello React</div>
+    <>
+    <Navbar/>
+
+    <Routes>
+      <Route  path="/" element={<Home/>} />
+      <Route  path="/login" element={<Login/>} />
+    </Routes>
+     
+     <Footer/>
+    </>
   );
 }
 
